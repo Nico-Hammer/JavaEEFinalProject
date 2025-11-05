c12a594f7160b40dd34eb90fac295d59ca77c38a
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-05 11:07:59 -0600

    used spring intializr to create a template project with all required dependencies (springweb, jpa, thymeleaf, etc)

---
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/FinalProjectCampusJobBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalProject_CampusJobBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCampusJobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCampusJobBoardApplication.class, args);
+	}
+
+}
