15bb19de7449210061650fd9bbde41ff1fa1ada2
Chase Petrowsky <chasepetrowsky@gmail.com>
2025-11-27 15:57:01 -0700

    all fields and connections for the Job entity. Must create package and files for enums for all entities.

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/entity/Job.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/entity/Job.java
@@ -1,4 +1,46 @@
 package com.example.FinalProject_CampusJobBoard.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "job")
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long job_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employer_id", nullable = false)
+    private User employer;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(length = 100)
+    private String location;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal salary;
+
+    @Column(length = 50)
+    private String category;
+
+
+    private LocalDate deadline;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private JobStatus status = JobStatus.PENDING;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
 }
