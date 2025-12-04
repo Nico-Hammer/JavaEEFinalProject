5e674de180041589f6975af6acc7b1d990c48369
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-04 09:49:28 -0700

    updated jobnotfound exception

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/JobNotFoundException.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/JobNotFoundException.java
@@ -3,10 +3,7 @@ package com.example.FinalProject_CampusJobBoard.exception;
 import com.example.FinalProject_CampusJobBoard.entity.Job;
 
 public class JobNotFoundException extends RuntimeException {
-    public JobNotFoundException(Job job){
-        super("Job with " + job.getTitle() + " not found.");
-    }
-    public JobNotFoundException(Job job){
-        super("Job with id " + job.getId() + " not found");
+    public JobNotFoundException(String message){
+        super(message);
     }
 }
--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/UserNotFoundException.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.FinalProject_CampusJobBoard.exception;
 import com.example.FinalProject_CampusJobBoard.entity.User;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(User user){
-        super();
+    public UserNotFoundException(String message){
+        super(message);
     }
 }
