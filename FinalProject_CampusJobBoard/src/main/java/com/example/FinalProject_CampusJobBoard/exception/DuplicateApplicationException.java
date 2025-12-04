0dadb14e8da4e9588c3c80b2e5b1309d701dd9db
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-04 09:49:54 -0700

    updated duplicateapplication exception

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/DuplicateApplicationException.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/DuplicateApplicationException.java
@@ -3,7 +3,7 @@ package com.example.FinalProject_CampusJobBoard.exception;
 import com.example.FinalProject_CampusJobBoard.entity.JobApplication;
 
 public class DuplicateApplicationException extends RuntimeException {
-    public DuplicateApplicationException(JobApplication jobApplication){
-        super("Duplicate job application with id " + jobApplication.getId());
+    public DuplicateApplicationException(String message){
+        super(message);
     }
 }
