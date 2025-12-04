1e0e0ac0dc40a7f22451c4694019cd2bab2ac494
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-04 09:48:23 -0700

    updated usernotfound exception

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/UserNotFoundException.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/UserNotFoundException.java
@@ -4,10 +4,6 @@ import com.example.FinalProject_CampusJobBoard.entity.User;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(User user){
-        super("User " + user.getName() + " not found.");
-    }
-
-    public UserNotFoundException(User user){
-        super("User with id " + user.getId() + " not found");
+        super();
     }
 }
