5b45e0c58b081ee6be5ddcbfc90280a29d54d30c
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-07 16:46:16 -0700

    jwtutil implementation

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/Security/jwt/JwtUtil.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/Security/jwt/JwtUtil.java
@@ -1,4 +1,51 @@
 package com.example.FinalProject_CampusJobBoard.Security.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtUtil {
+
+    private static final String SECRET_KEY = "08e52089708c88110d8c7e22916410db7a7a00abbe8227dfc3c661397e0ebc5a71b993f674c181b13b8acac225e5a1ab000cfa4eb2fba2bc076c5f83a872f6f2";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String generateToken(org.springframework.security.core.userdetails.UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.toList()));
+
+        // create token
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, org.springframework.security.core.userdetails.UserDetails userDetails) {
+        return (extractUsername(token).equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
 }
