2d0228871c7248395c63b6d6b992dd12a78e7375
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-07 16:41:21 -0700

    added base files

---
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/Security/jwt/JwtAuthFilter.java
@@ -0,0 +1,4 @@
+package com.example.FinalProject_CampusJobBoard.Security.jwt;
+
+public class JwtAuthFilter {
+}
---
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/Security/user/CustomUserDetails.java
@@ -0,0 +1,4 @@
+package com.example.FinalProject_CampusJobBoard.Security.user;
+
+public class CustomUserDetails {
+}
