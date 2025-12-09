43323e6e8e1648251a3b49b3dc547d3fea7e48cb
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-09 13:59:02 -0700

    renamed unauthorizeduser exception

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/exception/UnauthorizedUser.java
+++
@@ -1,7 +0,0 @@
-package com.example.FinalProject_CampusJobBoard.exception;
-
-public class UnauthorizedUser extends RuntimeException {
-    public UnauthorizedUser(String message) {
-        super(message);
-    }
-}
