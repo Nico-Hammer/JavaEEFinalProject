ef4caec49c6c0bf92e79fd1088644af463b250a3
Chase Petrowsky <chasepetrowsky@gmail.com>
2025-12-10 14:11:08 -0700

    Added StudentController services

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/controller/StudentController.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/controller/StudentController.java
@@ -1,4 +1,18 @@
 package com.example.FinalProject_CampusJobBoard.controller;
 
+import com.example.FinalProject_CampusJobBoard.service.ApplicationService;
+import com.example.FinalProject_CampusJobBoard.service.JobService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+    private final JobService jobService;
+    private final ApplicationService applicationService;
+
+    public StudentController(JobService jobService, ApplicationService applicationService) {
+        this.jobService = jobService;
+        this.applicationService = applicationService;
+    }
 }
