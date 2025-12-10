1386bed738aaf5207125529569659276b06edfab
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-10 12:27:30 -0700

    admin controller decline job POST mapping

--- FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/controller/AdminController.java
+++ FinalProject_CampusJobBoard/src/main/java/com/example/FinalProject_CampusJobBoard/controller/AdminController.java
@@ -3,14 +3,12 @@ package com.example.FinalProject_CampusJobBoard.controller;
 import com.example.FinalProject_CampusJobBoard.entity.Job;
 import com.example.FinalProject_CampusJobBoard.entity.User;
 import com.example.FinalProject_CampusJobBoard.enums.JobStatus;
-import com.example.FinalProject_CampusJobBoard.service.ApplicationService;
-import com.example.FinalProject_CampusJobBoard.service.JobService;
+import com.example.FinalProject_CampusJobBoard.exception.JobNotFoundException;
 import com.example.FinalProject_CampusJobBoard.service.JobServiceImpl;
 import com.example.FinalProject_CampusJobBoard.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +37,19 @@ public class AdminController {
         List<Job> pendingJobs = jobService.findByStatus(JobStatus.PENDING);
         model.addAttribute("pendingJobs",pendingJobs);
         return "admin/pending-jobs";
+    }
+
+    // Decline job posting
+    @PostMapping("/pending-jobs/{id}/decline")
+    public String declineJobPosting(@PathVariable Long id, @ModelAttribute("job") Job declinedJob){
+        Job foundJob = jobService.findById(id);
+        if(foundJob == null){
+            throw new JobNotFoundException("Job with ID " + id + " not found");
+        }
+        if(declinedJob.getStatus() != JobStatus.REJECTED) {
+            foundJob.setStatus(JobStatus.REJECTED);
+        }
+        jobService.saveJob(foundJob);
+        return "redirect:/admin/pending-jobs";
     }
 }
