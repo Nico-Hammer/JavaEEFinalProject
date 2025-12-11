6d55bb7fd4bc003f42864bf92cd495cff882d432
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-11 15:58:20 -0700

    jobapplication repo findbyjob test

--- FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/repository/JobApplicationRepositoryTest.java
+++ FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/repository/JobApplicationRepositoryTest.java
@@ -40,9 +40,11 @@ class JobApplicationRepositoryTest {
         job = new Job();
         job.setJob_id(1l);
         student = new User();
+
         student.setPassword("somePass");
         student.setEmail("e@mail.com");
         student.setFullName("test");
+
         application = new JobApplication();
         application.setJob(job);
         application.setStudent(student);
@@ -55,6 +57,7 @@ class JobApplicationRepositoryTest {
         repo.deleteAll();
         jobRepo.deleteAll();
         userRepo.deleteAll();
+
         job = null;
         student = null;
         application = null;
@@ -62,16 +65,28 @@ class JobApplicationRepositoryTest {
 
     @Test
     void testFindByStatus() {
+        /* create the jobapplication list and setup mock repo and return result */
         List<JobApplication> applications = Arrays.asList(application);
         when(repo.findByStatus(JobApplicationStatus.SUBMITTED)).thenReturn(applications);
+        /* get the job application by status and make sure that it has the expected status */
         List<JobApplication> foundApplications = repo.findByStatus(JobApplicationStatus.SUBMITTED);
         assertThat(foundApplications).hasSize(1);
         assertThat(foundApplications.getFirst().getStatus()).isEqualTo(JobApplicationStatus.SUBMITTED);
+        /* make sure that the repository function is actually called */
         verify(repo,times(1)).findByStatus(JobApplicationStatus.SUBMITTED);
     }
 
     @Test
     void testFindByJob() {
+        /* create the jobapplication list and setup mock repo and return result */
+        List<JobApplication> applications = Arrays.asList(application);
+        when(repo.findByJob(job)).thenReturn(applications);
+        /* get the job application by job and make sure that it has the expected job application id */
+        List<JobApplication> foundApplications = repo.findByJob(job);
+        assertThat(foundApplications).hasSize(1);
+        assertThat(foundApplications.getFirst().getJobApplication_id()).isEqualTo(application.getJobApplication_id());
+        /* make sure that the repository function is actually called */
+        verify(repo,times(1)).findByJob(job);
     }
 
     @Test
