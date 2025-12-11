de69e67c0e9f52d4ec4f7cde1b7b60d73791eb71
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-11 13:46:11 -0700

    setup roles and created findbyemail test for user repo

--- FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/repository/UserRepositoryTest.java
+++ FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/repository/UserRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.example.FinalProject_CampusJobBoard.repository;
 
+import com.example.FinalProject_CampusJobBoard.entity.Role;
 import com.example.FinalProject_CampusJobBoard.entity.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,11 +18,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRepositoryTest {
     @Autowired
     private UserRepository repo;
+    @Autowired
+    private RoleRepository roleRepo;
 
     User user = new User();
+    Set<Role> studentRoles = new HashSet<>();
+    Set<Role> employerRoles = new HashSet<>();
+    Set<Role> adminRoles = new HashSet<>();
+    Role userRole = new Role();
+    Role studentRole = new Role();
+    Role employerRole = new Role();
+    Role adminRole = new Role();
     @BeforeEach
     void setupUser(){
         repo.deleteAll();
+        userRole.setName("USER");
+        studentRole.setName("STUDENT");
+        employerRole.setName("EMPLOYER");
+        adminRole.setName("ADMIN");
+        userRole = roleRepo.save(userRole);
+        studentRole = roleRepo.save(studentRole);
+        employerRole = roleRepo.save(employerRole);
+        adminRole = roleRepo.save(adminRole);
+        studentRoles.add(userRole);
+        studentRoles.add(studentRole);
+        employerRoles.add(userRole);
+        employerRoles.add(employerRole);
+        adminRoles.add(userRole);
+        adminRoles.add(adminRole);
         user.setFullName("John Test");
         user.setEmail("John@email.com");
         user.setPassword("SomePassword");
@@ -26,6 +53,9 @@ class UserRepositoryTest {
     @AfterEach
     void teardownUser(){
         user = null;
+        studentRoles = null;
+        employerRoles = null;
+        adminRoles = null;
         repo.deleteAll();
     }
     @Test
@@ -35,11 +65,18 @@ class UserRepositoryTest {
         User foundUser = repo.findByFullName(savedUser.getFullName()).orElse(null);
         assertThat(foundUser).isNotNull();
         assertThat(foundUser.getFullName()).isEqualTo("John Test");
-        System.out.println("SaveAndFindByName execute successfully with name of " + foundUser.getFullName());
+        System.out.println("SaveAndFindByName executed successfully with name of " + foundUser.getFullName());
     }
 
     @Test
-    void findByEmail() {
+    void testSaveAndFindByEmail() {
+        User savedUser = repo.save(user);
+        assertThat(savedUser.getUser_id()).isNotNull();
+        User foundUser = repo.findByEmail(savedUser.getEmail()).orElse(null);
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getEmail()).isEqualTo("John@email.com");
+        System.out.println("SaveAndFindByEmail executed successfully with email of " + foundUser.getEmail());
+
     }
 
     @Test
