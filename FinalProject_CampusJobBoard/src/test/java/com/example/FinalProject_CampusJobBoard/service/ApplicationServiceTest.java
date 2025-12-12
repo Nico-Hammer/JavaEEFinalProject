5145f6b553181857e50b403f2711130f1e7812b8
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-12 12:44:28 -0700

    findall test, test renaming and further setup

--- FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/service/ApplicationServiceTest.java
+++ FinalProject_CampusJobBoard/src/test/java/com/example/FinalProject_CampusJobBoard/service/ApplicationServiceTest.java
@@ -18,7 +18,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -39,8 +43,11 @@ class ApplicationServiceTest {
     private UserService userService;
 
     private JobApplication application;
+    private JobApplication application2;
     private Job job;
+    private Job job2;
     private User student;
+    private User student2;
     private JobStatus jobStatus;
     private JobApplicationStatus jobApplicationStatus;
 
@@ -56,6 +63,11 @@ class ApplicationServiceTest {
         student.setEmail("student@mail.com");
         student.setFullName("Student");
 
+        student2 = new User();
+        student2.setPassword("Pomepass");
+        student2.setEmail("student2@mail.com");
+        student2.setFullName("Second");
+
         job = new Job();
         job.setJob_id(1l);
         job.setCategory("job");
@@ -63,11 +75,24 @@ class ApplicationServiceTest {
         job.setTitle("Job");
         job.setStatus(jobStatus);
 
+        job2 = new Job();
+        job2.setJob_id(2l);
+        job2.setCategory("different");
+        job2.setEmployer(student2);
+        job2.setTitle("Job2");
+        job2.setStatus(jobStatus);
+
         application = new JobApplication();
         application.setJobApplication_id(1l);
         application.setJob(job);
         application.setStudent(student);
         application.setStatus(jobApplicationStatus);
+
+        application2 = new JobApplication();
+        application2.setJobApplication_id(2l);
+        application2.setJob(job2);
+        application2.setStudent(student2);
+        application2.setStatus(jobApplicationStatus);
     }
 
     @AfterEach
@@ -79,41 +104,62 @@ class ApplicationServiceTest {
         service = null;
         jobService = null;
         userService = null;
+
+        jobStatus = null;
+        jobApplicationStatus = null;
+
+        student = null;
+        student2 = null;
+        job = null;
+        job2 = null;
+        application = null;
+        application2 = null;
     }
 
     @Test
-    void findAll() {
+    void testFindAll() {
+        /* create the list of jobs and configure mockito behaviour */
+        List<JobApplication> jobApplications = List.of(application,application2);
+        when(service.findAll()).thenReturn(jobApplications);
+        /* get the result and make sure its what was expected */
+        List<JobApplication> foundJobApplications = service.findAll();
+        assertThat(foundJobApplications).isNotNull();
+        assertThat(foundJobApplications).hasSize(2);
+        assertThat(foundJobApplications).contains(application);
+        assertThat(foundJobApplications).contains(application2);
+        /* make sure the service method was actually called */
+        verify(service,times(1)).findAll();
     }
 
     @Test
-    void findById() {
+    void testFindById() {
     }
 
     @Test
-    void save() {
+    void testSave() {
     }
 
     @Test
-    void deleteById() {
+    void testDeleteById() {
     }
 
     @Test
-    void findByStudent() {
+    void testFindByStudent() {
     }
 
     @Test
-    void existsByJobAndStudent() {
+    void testExistsByJobAndStudent() {
     }
 
     @Test
-    void createApplication() {
+    void testCreateApplication() {
     }
 
     @Test
-    void findByStatus() {
+    void testFindByStatus() {
     }
 
     @Test
-    void findByJob() {
+    void testFindByJob() {
     }
 }
